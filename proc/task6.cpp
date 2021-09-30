661138e606f7f871a05331f3db4196c7786935ad
Sasha <Sasha@ACER>
2021-09-30 14:48:24 +0300

    1

---
+++ proc/task6.cpp
@@ -0,0 +1,43 @@
+﻿// proc.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+//task6
+void DigitCountSum(int*pn,int*ps,int*pk ) {
+    int a;
+   
+   
+    while (*pn != 0) {
+        a = *pn;
+       *ps = *ps + a % 10;
+        *pn = *pn / 10;
+        a = (a % 10) / 10;
+        *pn = *pn - a;
+        *pk = *pk + 1;
+        
+    }
+    /*cout << "amount of numbers: " << *pk << endl;
+    cout << "sum of numbers: " << *ps << endl;*/
+  
+}
+
+int main()
+{
+    int n;
+    int k = 0;
+    int s = 0;
+    cout << "enter your number: ";
+    cin >> n;
+    int* pn = &n;
+    int* ps = &s; 
+    int* pk = &k;
+    DigitCountSum(&n,&s,&k);
+    cout << "amount of numbers: " << *pk << endl;
+    cout << "sum of numbers: " << *ps << endl;
+    cin.get();
+    cin.get();
+   
+    return 0;
+}
+
---
+++ proc12/task12.cpp
@@ -0,0 +1,46 @@
+﻿// proc12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+void  SortIns3(int* pa, int* pb, int* pc) {
+    int x;
+    if (*pb < *pa &&  *pb < *pc) {
+        x = *pa;
+        *pa = *pb;
+        *pb = x;
+   }
+    if (*pc < *pa && *pc < *pb) {
+        x = *pa;
+        *pa = *pc;
+        *pc = x;
+    }
+    if (*pc < *pb) {
+        x = *pb;
+        *pb = *pc;
+        *pc = x;
+    }
+}
+int main()
+{
+    int A, B, C;    
+    for (int i = 1; i < 3; i++) {
+        cout << "enter A: ";
+        cin >> A;
+        cout << "enter B: ";
+        cin >> B;
+        cout << "enter C: ";
+        cin >> C;
+        int* pa = &A;
+        int* pb = &B;
+        int* pc = &C;
+        SortIns3(&A, &B, &C);
+        cout << " A: " << *pa << " B: " << *pb << " C: " << *pc<<endl;
+        
+
+    }
+    cin.get();
+    cin.get();
+    return 0;
+}
+
